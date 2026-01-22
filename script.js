9b5b934e628db4d14c467b4d2cbad9325fae9555
Petr klíma <ytbpetrak7@gmail.com>
2026-01-22 18:10:17 +0100

    konecne pres pul h

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function rozbalit() {
-        document.querySelector(".rozbalovaci")
+    document.querySelector(".rozbalovaci")
         .classList.toggle("rozbaleno")
+}
+
+function overitkod() {
+    let kod = document.getElementById("kod");
+    if (kod.value == "kamosi") {
+        window.location = '/P7.html';
+    } else {
+        alert("spatny kod lopato")
     }
+}
